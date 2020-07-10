9b1f9418bd492acc83818b5bd29e35f8a60aedc8
jamiebird119 <jamie.bird119@gmail.com>
2020-07-10 10:21:33 +0000

    Add recipe form complete with add ingredient js functionality

--- static/js/run.js
+++ static/js/run.js
@@ -1,3 +1,34 @@
 $(document).ready(function(){
         $('select').material_select();
+});
+function add_ingredients(){
+    var el = $("#additional_ingredients");
+    el.append(`<div class="row added">
+			<div class="input-field col s5">
+				<input
+          id="ingredient_name"
+          type="text"
+          class="validate"
+        />
+				<label for="ingredient_name">Ingredient Name</label>
+			</div>
 
+			<div class="input-field col s5">
+				<input
+          id="ingredient_quantity"
+          type="text"
+          class="validate"
+        />
+				<label for="ingredient_quantity">Ingredient Quantity</label>
+			</div>
+            <div class="col s2"> 
+            <a class="btn-floating btn-large waves-effect waves-light red"
+				onclick="remove_ingredients()">
+                <i class="material-icons">remove</i>
+                </a>
+                </div>
+                </div>`);
+}
+function remove_ingredients(){
+    var el = $('.added').last()
+    el.remove();}
