2e777b327ce98487dd34531c40a7e27142b61b53
jamiebird119 <jamie.bird119@gmail.com>
2020-07-11 11:37:18 +0000

    Added login and register functionality and error messages

--- app.py
+++ app.py
@@ -6,6 +6,7 @@ from bson.objectid import ObjectId
 if path.exists('env.py'):
     import env
 
+
 app = Flask(__name__)
 
 app.config["MONGO_DBNAME"] = os.environ.get("MONGO_DBNAME")
@@ -48,16 +49,34 @@ def insert_recipe():
 
 @app.route('/register')
 def register():
-    return render_template("register.html")
+    return render_template("register.html", alert='')
+
+
+@app.route('/login', methods=["POST"])
+def login():
+    user = mongo.db.users
+    user_login = {"username": request.form.get('username'),
+                  "password": request.form.get('password')}
+    user_details = user.find_one(user_login)
+    if user_details:
+        return render_template('userhome.html',
+                               user=user_details)
+    else:
+        return render_template('index.html',
+                               alert="Username or password not recognised. Please try again.")
 
 
 @app.route('/adduser', methods=["POST"])
 def adduser():
     user = mongo.db.users
-    user.insert_one(request.form.to_dict())
-    return render_template("loggedin.html", user=request.form.to_dict())
-
-
+    username = request.form.get("username")
+    if user.find_one({"username": username}) == '':
+        user.insert_one(request.form.to_dict())
+        return render_template("loggedin.html", user=request.form.to_dict())
+    else:
+        exists = "Username already exists. Log in or use another username."
+        return render_template('register.html',
+                               alert=exists)
 
 
 if __name__ == '__main__':
