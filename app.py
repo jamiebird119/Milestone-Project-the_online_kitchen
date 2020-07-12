14674078087ff81dcb97ea8ea519efa522efdab8
jamiebird119 <jamie.bird119@gmail.com>
2020-07-12 09:55:47 +0000

    Add recipe function saving to database

--- app.py
+++ app.py
@@ -31,28 +31,35 @@ def insert_recipe():
     recipes = mongo.db.recipes
     name = request.form.get("recipe_name")
     added_by = request.form.get("added_by")
-    ingredients = request.form.getlist("ingredient")
-    print(ingredients)
-    ingredients_quantity = request.form.getlist("ingredient_quantity")
-    print(ingredients_quantity)
+    ingredients = zip(request.form.getlist("ingredient"),
+                      request.form.getlist("ingredient_quantity"))
     method = request.form.get("method")
     difficulty = request.form.get("difficulty")
     cooking_time = request.form.get("cooking_time")
     recipe = {"recipe_name": name, "added_by": added_by,
               "method": method,
-              "ingredients": ingredients,
-              "ingredients_quantity": ingredients_quantity,
+              "ingredients": list(ingredients),
               "difficulty": difficulty,
               "cooking_time": cooking_time}
+    recipes.insert_one(recipe)
+    user = mongo.db.user
+    user_details = user.find({"username": added_by})
+    if user_details.myrecipes:
+        user_recipes = user_details.myrecipes
+        user.update_one({"username": added_by}, {
+                        "$set": {"myrecipes": user_recipes.append(name)}})
+    else: 
+        user.update_one({"username": added_by}, {
+                        "$set": {"myrecipes": name}})
     return render_template('recipe.html', recipe=recipe)
 
 
-@app.route('/register')
+@ app.route('/register')
 def register():
     return render_template("register.html", alert='')
 
 
-@app.route('/login', methods=["POST"])
+@ app.route('/login', methods=["POST"])
 def login():
     user = mongo.db.users
     user_login = {"username": request.form.get('username'),
@@ -66,7 +73,7 @@ def login():
                                alert="Username or password not recognised. Please try again.")
 
 
-@app.route('/adduser', methods=["POST"])
+@ app.route('/adduser', methods=["POST"])
 def adduser():
     user = mongo.db.users
     username = request.form.get("username")
--- static/js/run.js
+++ static/js/run.js
@@ -1,39 +1,41 @@
-$(document).ready(function(){
-        $('select').material_select();
+$(document).ready(function () {
+  $("select").material_select();
 });
 
-function add_ingredients(){
-    var el = $("#additional_ingredients");
-    el.append(`<div class="row added">
-			<div class="input-field col s5">
+function add_ingredients() {
+  var el = $(".ingredients");
+  el.append(`
 				<input
-          id="ingredient[]"
-          name="ingredient[]"
+          id="ingredient"
+          name="ingredient"
           type="text"
-          class="validate"
+          class="validate added_ingredient"
         />
-				<label for="ingredient">Ingredient Name</label>
-			</div>
+				
+			`);
 
-			<div class="input-field col s5">
-				<input
-          id="ingredient_quantity[]"
+  var el_2 = $(".quantity");
+  el_2.append(`<input
+          id="ingredient_quantity"
           type="text"
-          class="validate"
-          name="ingredient_quantity[]"
+          class="validate added_quantity"
+          name="ingredient_quantity"
         />
-				<label for="ingredient_quantity">Ingredient Quantity</label>
-			</div>
-            <div class="col s2"> 
-            <a class="btn-floating btn-large waves-effect waves-light red"
+				`);
+    var button = $("#additional");
+    button.append(`<div class="col s12 added"><a class="btn-floating btn-large waves-effect waves-light red"
 				onclick="remove_ingredients()">
                 <i class="material-icons">remove</i>
-                </a>
-                </div>
-                </div>`);
-    }
-    
+                </a></div>
+                `);
+            
 }
-function remove_ingredients(){
-    var el = $('.added').last()
-    el.remove();}
+
+function remove_ingredients() {
+  var el = $(".added_ingredient").last();
+  var el_2 = $(".added_quantity").last();
+  var button = $(".added").last();
+  el.remove();
+  el_2.remove();
+  button.remove();
+}
